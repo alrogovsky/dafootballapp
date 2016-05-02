abfe4d3c8f87b46d4ee8b0c77dcde6b41f83244f
alrogovsky <alex@rogovsky.ru>
2016-05-02 13:06:08 +0300

    loading activity

---
+++ app/src/main/java/com/sashqua/wowser/LoadingActivity.java
@@ -0,0 +1,26 @@
+package com.sashqua.wowser;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class LoadingActivity extends AppCompatActivity {
+
+    private final int WAIT_TIME = 2500;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent(LoadingActivity.this, MainActivity.class);
+                LoadingActivity.this.startActivity(mainIntent);
+                LoadingActivity.this.finish();
+            }
+        }, WAIT_TIME);
+    }
+}
